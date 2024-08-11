7300236e455be14de0ebb81f2088aac2cbf9ed41
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-08-11 18:01:54 +0300

    Bug Fix

--- MultiShop/Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
@@ -1,9 +1,12 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace MultiShop.Catalog.Entities
 {
     public class ProductImage
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string ProductImageId { get; set; }
         public string Image1 { get; set; }
         public string Image2 { get; set; }
