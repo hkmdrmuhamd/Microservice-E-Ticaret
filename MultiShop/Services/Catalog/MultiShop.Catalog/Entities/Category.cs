86e4a26f698561c27aa2b08a08620c48fb756f84
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-08-11 16:41:49 +0300

    Catalog Entities Added

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
+{
+    public class Category
+    {
+        [BsonId] //MongoDB bu class için bir ObjectId oluşturacak
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
