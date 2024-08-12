eae0961d689751319378ac663c7a79bbc9232e5e
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-08-12 17:49:26 +0300

    Catalog API Controllers Created

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Services.CategoryServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var values = await _categoryService.GettAllCategoryAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategoryById(string id)
+        {
+            var values = await _categoryService.GetByIdCategoryAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            /*
+             Category category = new Category();
+             {
+                 category.cateCategoryName = createCategoryDto.CategoryName,
+                 category.Description = createCategoryDto.Description,
+                 category.ImageUrl = createCategoryDto.ImageUrl
+             }; 
+            Mapping işlemi yaptığımız için bu işlemlere gerek kalmadan aşağıdaki gibi yapabiliriz.             
+            */
+            await _categoryService.CreateCategoryAsync(createCategoryDto);
+            return Ok("Kategori başarıyla eklendi.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            await _categoryService.UpdateCategoryAsync(updateCategoryDto);
+            return Ok("Kategori başarıyla güncellendi.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            await _categoryService.DeleteCategoryAsync(id);
+            return Ok("Kategori başarıyla silindi.");
+        }
+        
+    }
+}
---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Controllers/ProductDetailsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductDetailDtos;
+using MultiShop.Catalog.Services.ProductDetailServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductDetailsController : ControllerBase
+    {
+        private readonly IProductDetailService _productDetailsService;
+
+        public ProductDetailsController(IProductDetailService productDetailsService)
+        {
+            _productDetailsService = productDetailsService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductDetailsList()
+        {
+            var values = await _productDetailsService.GettAllProductDetailAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductDetailsById(string id)
+        {
+            var values = await _productDetailsService.GetByIdProductDetailAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProductDetails(CreateProductDetailDto createProductDetailsDto)
+        {
+            await _productDetailsService.CreateProductDetailAsync(createProductDetailsDto);
+            return Ok("Ürün detayı başarıyla eklendi.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductDetails(UpdateProductDetailDto updateProductDetailsDto)
+        {
+            await _productDetailsService.UpdateProductDetailAsync(updateProductDetailsDto);
+            return Ok("Ürün detayı başarıyla güncellendi.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductDetails(string id)
+        {
+            await _productDetailsService.DeleteProductDetailAsync(id);
+            return Ok("Ürün detayı başarıyla silindi.");
+        }
+    }
+}
