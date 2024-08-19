343603f1385d0d00e53e1612f1e05a9f1744be38
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-08-19 20:56:05 +0300

    CQRS OrderDetail

---
+++ MultiShop/Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/UpdateOrderDetailCommandHanler.cs
@@ -0,0 +1,33 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class UpdateOrderDetailCommandHanler
+    {
+        private readonly IRepository<OrderDetail> _repository;
+
+        public UpdateOrderDetailCommandHanler(IRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateOrderDetailCommand request)
+        {
+            var orderDetail = await _repository.GetByIdAsync(request.OrderDetailId);
+            orderDetail.ProductId = request.ProductId;
+            orderDetail.ProductName = request.ProductName;
+            orderDetail.ProductPrice = request.ProductPrice;
+            orderDetail.ProductAmount = request.ProductAmount;
+            orderDetail.ProductTotalPrice = request.ProductTotalPrice;
+            orderDetail.OrderingId = request.OrderingId;
+            await _repository.UpdateAsync(orderDetail);
+        }
+    }
+}
