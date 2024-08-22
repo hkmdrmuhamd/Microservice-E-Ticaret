4e3ea519761ca58b6cc9eea60251b3df509184c6
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-08-23 00:15:26 +0300

    Mediator

---
+++ MultiShop/Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Queries/OrderingQueries/GetOrderingQuery.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using MultiShop.Order.Application.Features.Mediator.Results.OrderingResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries
+{
+    
+    public class GetOrderingQuery : IRequest<List<GetOrderingQueryResult>>
+//IRequest ile bunun bir istek olduğunu belirtiyoruz. IRequestHandler ile de bu isteği karşılayacak olan sınıfı belirtiyoruz.
+// Bu yapı sayesinde her controller'da ayrı ayrı constructor ile sınıfı oluşturmak yerine MediatR'ın sunduğu IRequestHandler yapısını kullanarak sınıfı oluşturuyoruz.
+    {
+    }
+}
