0919340d892f6b2bf132ec8a1823f5f5fdc0184f
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-08-27 02:52:21 +0300

    Cargo Context Class Created

---
+++ MultiShop/Services/Cargo/MultiShop.Cargo.DataAccessLayer/Concrete/CargoContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.DataAccessLayer.Concrete
+{
+    public class CargoContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1441;initial Catalog=MultiShopCargoDb;User=*****;Password=*****");
+        }
+
+        public DbSet<CargoCompany> CargoCompanies { get; set; }
+        public DbSet<CargoCustomer> CargoCustomer { get; set; }
+        public DbSet<CargoDetail> CargoDetail { get; set; }
+        public DbSet<CargoOperation> CargoOperation { get; set; }
+    }
+}
