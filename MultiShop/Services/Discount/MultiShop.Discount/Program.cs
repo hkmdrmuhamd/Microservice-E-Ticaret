1a40a209118bb5a57d1e9e52297f4a7e86134afe
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-08-27 01:07:32 +0300

    Authentication and Authorization Configurations

--- MultiShop/Services/Discount/MultiShop.Discount/Program.cs
+++ MultiShop/Services/Discount/MultiShop.Discount/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using MultiShop.Discount.Context;
 using MultiShop.Discount.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerUrl"];
+    options.Audience = "ResourceDiscount";
+    options.RequireHttpsMetadata = false;
+});
 
 // Add services to the container.
 builder.Services.AddTransient<DapperContext>();
@@ -24,6 +32,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
