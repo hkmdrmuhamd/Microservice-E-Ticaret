a95c68cfcee6b7376470fa565f5efc7f27157e15
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-08-29 00:21:05 +0300

    Business Layer Abstract Folder

---
+++ MultiShop/Services/Cargo/MultiShop.Cargo.BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        void TInsert(T entity); //Başına T koymamızın sebebi Business katmanından geldiğini belirtmek içindir. Başında T olmayan Data Access katmanından gelendir.
+        void TUpdate(T entity);
+        void TDelete(int id);
+        T TGetById(int id);
+        List<T> TGetAll();
+    }
+}
