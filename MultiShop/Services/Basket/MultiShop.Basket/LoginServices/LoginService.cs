1874cae5ab607f06e8ee08ae278511c4a9985f48
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-08-29 16:34:08 +0300

    Get user ID from token in login process

---
+++ MultiShop/Services/Basket/MultiShop.Basket/LoginServices/LoginService.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.Basket.LoginServices
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;// Kullanıcı kimliğini almak için HttpContextAccessor kullanılır.
+
+        public LoginService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        
+        public string GetUserId => _httpContextAccessor.HttpContext.User.FindFirst("sub").Value; //sub ile jwt içerisindeki kullanıcı kimliği alınır.
+    }
+}
