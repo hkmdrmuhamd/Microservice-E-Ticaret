f6ca2a8d5302c16f94839c5f6eb6d81fcc0a8ebd
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-08-29 16:10:27 +0300

    Added Redis properties and configured Redis connection

---
+++ MultiShop/Services/Basket/MultiShop.Basket/Settings/RedisService.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+
+namespace MultiShop.Basket.Settings
+{
+    public class RedisService
+    {
+        public string _host { get; set; }
+        public int _port { get; set; }
+        private ConnectionMultiplexer _connectionMultiplexer; // ConnectionMultiplexer, Redis'e yapılan bağlantıları yöneten sınıftır
+        public RedisService(string host, int port)
+        {
+            _host = host;
+            _port = port;
+        }
+
+        public void Connect() => _connectionMultiplexer = ConnectionMultiplexer.Connect($"{_host}:{_port}"); // Redis'e bağlanma işlemi
+        
+        public IDatabase GetDb(int db = 0) => _connectionMultiplexer.GetDatabase(db);// Bu işlem Redis'e bağlı 0 numaralı veritabanına erişim sağlamak için kullanılır.
+    }
+}
