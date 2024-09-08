d9126d6d5bdb8e6ae4b5fc65d79f6573ea576258
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-09-08 17:55:26 +0300

    Category Controller and View

---
+++ MultiShop/Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.V0 = "Kategori İşlemleri";
+            ViewBag.V1 = "Ana Sayfa";
+            ViewBag.V2 = "Kategoriler";
+            ViewBag.V3 = "Kategori Listesi";
+            return View();
+        }
+    }
+}
