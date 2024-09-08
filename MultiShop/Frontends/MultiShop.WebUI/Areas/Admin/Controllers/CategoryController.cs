8da7fffe1302449d48beadf670b6887d444865d0
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-09-09 00:02:30 +0300

    Create Category and Delete Category Operations

--- MultiShop/Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ MultiShop/Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -2,10 +2,13 @@
 using Microsoft.AspNetCore.Mvc;
 using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [AllowAnonymous] // Herkes tarafından görüntülenebilir.
+    [Route("Admin/Category")] // URL'de kullanılacak isim. (Bu işlem bazı yönlendirmelerin yapılabilmesi için kullanılmıştır.)
     public class CategoryController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
@@ -15,7 +18,7 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
-        [AllowAnonymous] // Kategori listesi herkes tarafından görüntülenebilir.
+        [Route("Index")]
         public async Task<IActionResult> Index()
         {
             ViewBag.V0 = "Kategori İşlemleri";
@@ -30,6 +33,47 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
                 var jsonData = await responseMessage.Content.ReadAsStringAsync(); //Veri JSON formatına çevrildi.
                 var categories = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData); //JSON formatındaki veri ResultCategoryDto tipine çevrildi.
                 return View(categories);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("CreateCategory")]
+        public IActionResult CreateCategory()
+        {
+            ViewBag.V0 = "Kategori İşlemleri";
+            ViewBag.V1 = "Ana Sayfa";
+            ViewBag.V2 = "Kategoriler";
+            ViewBag.V3 = "Kategori Listesi";
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateCategory")]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCategoryDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json"); //Veri JSON formatına çevrildi.
+            var responseMessage = await client.PostAsync("https://localhost:7000/api/Categories", stringContent);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
+            }
+
+            return View();
+        }
+
+        [Route("DeleteCategory/{id}")]
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7000/api/Categories?id=" + id);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
             }
 
             return View();
