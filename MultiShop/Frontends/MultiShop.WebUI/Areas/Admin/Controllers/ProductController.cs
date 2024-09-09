a06e141aafad8446cfb4298472cd05e1fee557b0
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-09-09 03:02:40 +0300

    Product Controller and Product List

---
+++ MultiShop/Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+using Newtonsoft.Json;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    [Route("Admin/Product")]
+    public class ProductController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.V0 = "Ürün İşlemleri";
+            ViewBag.V1 = "Ana Sayfa";
+            ViewBag.V2 = "Ürünler";
+            ViewBag.V3 = "Ürün Listesi";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7000/api/Products");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var categories = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(categories);
+            }
+
+            return View();
+        }
+    }
+}
