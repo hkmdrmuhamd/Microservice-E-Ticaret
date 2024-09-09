28f862268ceb9a036a29025db5df9d60c944c2cf
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-09-09 21:05:08 +0300

    Feature Slider Services

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Services/FeatureSliderServices/FeatureSliderService.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Misc;
+using MultiShop.Catalog.Dtos.FeatureSliderDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.FeatureSliderServices
+{
+    public class FeatureSliderService : IFeatureSliderService
+    {
+        public Task CreateFeatureSliderAsync(CreateFeatureSliderDto createFeatureSliderDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteFeatureSliderAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task FeatureSliderChageStatusToFalse(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task FeatureSliderChageStatusToTrue(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultFeatureSliderDto>> GetAllFeatureSliderAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdFeatureSliderDto> GetByIdFeatureSliderAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateFeatureSliderAsync(UpdateFeatureSliderDto updateFeatureSliderDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Services/FeatureSliderServices/IFeatureSliderService.cs
@@ -0,0 +1,17 @@
+﻿using MultiShop.Catalog.Dtos.FeatureSliderDtos;
+using MultiShop.Catalog.Dtos.ProductDetailDtos;
+using MultiShop.Catalog.Entities;
+
+namespace MultiShop.Catalog.Services.FeatureSliderServices
+{
+    public interface IFeatureSliderService
+    {
+        Task CreateFeatureSliderAsync(CreateFeatureSliderDto createFeatureSliderDto);
+        Task UpdateFeatureSliderAsync(UpdateFeatureSliderDto updateFeatureSliderDto);
+        Task<List<ResultFeatureSliderDto>> GetAllFeatureSliderAsync();
+        Task<GetByIdFeatureSliderDto> GetByIdFeatureSliderAsync(string id);
+        Task DeleteFeatureSliderAsync(string id);
+        Task FeatureSliderChageStatusToTrue(string id);
+        Task FeatureSliderChageStatusToFalse(string id);
+    }
+}
