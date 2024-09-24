92ac92947ee6e78e545c4cec4f449fdd20c96de0
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-09-24 20:57:07 +0300

    Special Offer Services was Created

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Services/SpecialOfferServices/SpecialOfferService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.SpecialOfferDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.SpecialOfferServices
+{
+    public class SpecialOfferService : ISpecialOfferService
+    {
+        private readonly IMongoCollection<SpecialOffer> _specialOfferCollection;
+        private readonly IMapper _mapper;
+
+        public SpecialOfferService(IDatabaseSettings settings, IMapper mapper)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _specialOfferCollection = database.GetCollection<SpecialOffer>(settings.SpecialOfferCollectionName);
+            _mapper = mapper;
+        }
+
+        public async Task CreateSpecialOfferAsync(CreateSpecialOfferDto createSpecialOfferDto)
+        {
+            var specialOffer = _mapper.Map<SpecialOffer>(createSpecialOfferDto);
+            await _specialOfferCollection.InsertOneAsync(specialOffer);
+        }
+
+        public async Task DeleteSpecialOfferAsync(string id)
+        {
+            await _specialOfferCollection.DeleteOneAsync(x => x.SpecialOfferId == id);
+        }
+
+        public async Task<List<ResultSpecialOfferDto>> GetAllSpecialOfferAsync()
+        {
+            var specialOffers = await _specialOfferCollection.Find(x => true).ToListAsync();
+            return _mapper.Map<List<ResultSpecialOfferDto>>(specialOffers);
+        }
+
+        public async Task<GetByIdSpecialOfferDto> GetByIdSpecialOfferAsync(string id)
+        {
+            var specialOffer = await _specialOfferCollection.Find(x => x.SpecialOfferId == id).FirstOrDefaultAsync();
+            return _mapper.Map<GetByIdSpecialOfferDto>(specialOffer);
+        }
+
+        public async Task UpdateSpecialOfferAsync(UpdateSpecialOfferDto updateSpecialOfferDto)
+        {
+            var specialOffer = _mapper.Map<SpecialOffer>(updateSpecialOfferDto);
+            await _specialOfferCollection.FindOneAndReplaceAsync(x => x.SpecialOfferId == updateSpecialOfferDto.SpecialOfferId, specialOffer);
+        }
+    }
+}
