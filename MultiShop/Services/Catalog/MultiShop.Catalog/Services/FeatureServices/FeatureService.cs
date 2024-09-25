efbd8bbb73c20419a267fb0e4410a2dedb4016a0
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-09-25 15:03:45 +0300

    Feature Services Created

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Services/FeatureServices/FeatureService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.FeatureDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.FeatureServices
+{
+    public class FeatureService : IFeatureService
+    {
+        private readonly IMongoCollection<Feature> _featureCollection;
+        private readonly IMapper _mapper;
+
+        public FeatureService(IMapper mapper, IDatabaseSettings _databaseSettings)
+        {
+            var cliet = new MongoClient(_databaseSettings.ConnectionString);
+            var database = cliet.GetDatabase(_databaseSettings.DatabaseName);
+            _featureCollection = database.GetCollection<Feature>(_databaseSettings.FeatureCollectionName);
+            _mapper = mapper;
+        }
+
+        public async Task CreateFeatureAsync(CreateFeatureDto createFeatureDto)
+        {
+            await _featureCollection.InsertOneAsync(_mapper.Map<Feature>(createFeatureDto));
+        }
+
+        public async Task DeleteFeatureAsync(string id)
+        {
+            await _featureCollection.DeleteOneAsync(x => x.FeatureId == id);
+        }
+
+        public async Task<List<ResultFeatureDto>> GelAllFeaturesAsync()
+        {
+            var values = await _featureCollection.Find(x => true).ToListAsync();
+            return _mapper.Map<List<ResultFeatureDto>>(values);
+        }
+
+        public async Task<GetByIdFeatureDto> GetByIdFeatureAsync(string id)
+        {
+            var value = await _featureCollection.Find(x => x.FeatureId == id).FirstOrDefaultAsync();
+            return _mapper.Map<GetByIdFeatureDto>(value);
+        }
+
+        public async Task UpdateFeatureAsync(UpdateFeatureDto updateFeatureDto)
+        {
+            await _featureCollection.FindOneAndReplaceAsync(x => x.FeatureId == updateFeatureDto.FeatureId, _mapper.Map<Feature>(updateFeatureDto));
+        }
+    }
+}
