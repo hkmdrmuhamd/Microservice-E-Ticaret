f8f213554f80c9332c76d1a44e888e551631d706
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-09-25 15:04:09 +0300

    Feature Controller Created

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Controllers/FeaturesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.FeatureDtos;
+using MultiShop.Catalog.Services.FeatureServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly IFeatureService _featureService;
+
+        public FeaturesController(IFeatureService featureService)
+        {
+            _featureService = featureService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllFeaturesAsync()
+        {
+            var values = await _featureService.GelAllFeaturesAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdFeatureAsync(string id)
+        {
+            var value = await _featureService.GetByIdFeatureAsync(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFeatureAsync(CreateFeatureDto createFeatureDto)
+        {
+            await _featureService.CreateFeatureAsync(createFeatureDto);
+            return Ok("Öne çıkarılan eklendi.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateFeatureAsync(UpdateFeatureDto updateFeatureDto)
+        {
+            await _featureService.UpdateFeatureAsync(updateFeatureDto);
+            return Ok("Öne çıkarılan güncellendi.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteFeatureAsync(string id)
+        {
+            await _featureService.DeleteFeatureAsync(id);
+            return Ok("Öne çıkarılan silindi.");
+        }
+    }
+}
