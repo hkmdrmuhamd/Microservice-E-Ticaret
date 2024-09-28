92e006c73077a0580fde20dd932d67caab8e9238
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-09-28 15:13:07 +0300

    Responsive Categories UI

--- MultiShop/Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_CategoriesDefaultComponentPartial.cs
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_CategoriesDefaultComponentPartial.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _CategoriesDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _CategoriesDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient(); //Client oluşturuldu.
+            var responseMessage = await client.GetAsync("https://localhost:7000/api/Categories"); //API'den veri çekildi.
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); //Veri JSON formatına çevrildi.
+                var categories = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData); //JSON formatındaki veri ResultCategoryDto tipine çevrildi.
+                return View(categories);
+            }
             return View();
         }
     }
