4170993f0bbb819a65507907460434fdab00f996
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-09-30 18:33:49 +0300

    Dinamic Feature Products View Component

--- MultiShop/Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsComponentParital.cs
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsComponentParital.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _FeatureProductsComponentParital : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FeatureProductsComponentParital(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7000/api/Products");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var products = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(products);
+            }
             return View();
         }
     }
