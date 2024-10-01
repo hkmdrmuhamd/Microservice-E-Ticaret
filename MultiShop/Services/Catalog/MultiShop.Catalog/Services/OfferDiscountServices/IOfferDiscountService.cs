08e10f9bd4d98ca3a9abd8822b5060b4ecdde37a
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-10-01 19:11:16 +0300

    Offer Discount Services Created

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Services/OfferDiscountServices/IOfferDiscountService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.Catalog.Dtos.OfferDiscountDtos;
+
+namespace MultiShop.Catalog.Services.OfferDiscountServices
+{
+    public interface IOfferDiscountService
+    {
+        Task<List<ResultOfferDiscountDto>> GettAllOfferDiscountAsync();
+        Task<GetByIdOfferDiscountDto> GetByIdOfferDiscountAsync(string id); 
+        Task CreateOfferDiscountAsync(CreateOfferDiscountDto createOfferDiscountDto);
+        Task UpdateOfferDiscountAsync(UpdateOfferDiscountDto updateOfferDiscountDto);
+        Task DeleteOfferDiscountAsync(string id);
+    }
+}
