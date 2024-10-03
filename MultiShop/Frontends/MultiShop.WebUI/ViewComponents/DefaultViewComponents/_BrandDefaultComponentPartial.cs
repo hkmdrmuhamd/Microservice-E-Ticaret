c04952d63e6143cd7f7cb0198ec8c365d5b60644
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-10-03 03:09:38 +0300

    Dinamic Brand View Component

---
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_BrandDefaultComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.BrandDtos;
+using Newtonsoft.Json;
+
+namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _BrandDefaultComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BrandDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7000/api/Brands");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var brands = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+                return View(brands);
+            }
+
+            return View();
+        }
+    }
+}
