502079f1b704e94f7a449aee20d564022d4c6944
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-10-03 03:05:39 +0300

    Brand Mapping Operations

--- MultiShop/Services/Catalog/MultiShop.Catalog/Mapping/GeneralMapping.cs
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Mapping/GeneralMapping.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using MultiShop.Catalog.Dtos.BrandDtos;
 using MultiShop.Catalog.Dtos.CategoryDtos;
 using MultiShop.Catalog.Dtos.FeatureDtos;
 using MultiShop.Catalog.Dtos.FeatureSliderDtos;
@@ -55,6 +56,11 @@ namespace MultiShop.Catalog.Mapping
             CreateMap<OfferDiscount, GetByIdOfferDiscountDto>().ReverseMap();
             CreateMap<OfferDiscount, CreateOfferDiscountDto>().ReverseMap();
             CreateMap<OfferDiscount, UpdateOfferDiscountDto>().ReverseMap();
+
+            CreateMap<Brand, ResultBrandDto>().ReverseMap();
+            CreateMap<Brand,  GetByIdBrandDto>().ReverseMap();
+            CreateMap<Brand,  CreateBrandDto>().ReverseMap();
+            CreateMap<Brand,  UpdateBrandDto>().ReverseMap();
         }
     }
 }
