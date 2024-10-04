e2418ca30a91d42167b24ae99ebf668d3b09d4e2
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-10-04 18:55:17 +0300

    Api Url Update

--- MultiShop/Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListComponentPartial.cs
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListComponentPartial.cs
@@ -17,7 +17,7 @@ namespace MultiShop.WebUI.ViewComponents.ProductListViewComponents
         {
             id = "66de319fe63c778a599935fe";
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7000/api/ProductsWithCategoryByCategoryId?id=" + id);
+            var responseMessage = await client.GetAsync("https://localhost:7000/api/Products/ProductsWithCategoryByCategoryId?id=" + id);
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
