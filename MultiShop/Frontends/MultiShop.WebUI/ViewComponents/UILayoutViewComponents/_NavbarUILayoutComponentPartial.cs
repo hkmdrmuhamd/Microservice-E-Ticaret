6935b60b9c1c20439a30ec38f5d3790ff71f0036
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-10-04 19:32:43 +0300

    Making Category Names Dynamic in Navbar

--- MultiShop/Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
 {
     public class _NavbarUILayoutComponentPartial: ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _clientFactory;
+
+        public _NavbarUILayoutComponentPartial(IHttpClientFactory clientFactory)
         {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _clientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7000/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var content = await responseMessage.Content.ReadAsStringAsync();
+                var categorys = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(content);
+                return View(categorys);
+            }
             return View();
         }
     }
