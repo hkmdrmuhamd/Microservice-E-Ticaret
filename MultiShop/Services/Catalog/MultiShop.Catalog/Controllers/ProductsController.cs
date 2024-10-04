38e64b64f31951015f4913424f07914d40a6e8df
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-10-04 18:52:22 +0300

    Created Api to Get Products by CategoryId

--- MultiShop/Services/Catalog/MultiShop.Catalog/Controllers/ProductsController.cs
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Controllers/ProductsController.cs
@@ -58,5 +58,12 @@ namespace MultiShop.Catalog.Controllers
             var values = await _ProductService.GetProductsWithCategoryAsync();
             return Ok(values);
         }
+
+        [HttpGet("ProductsWithCategoryByCategoryId")]
+        public async Task<IActionResult> ProductsWithCategoryByCategoryId(string id)
+        {
+            var values = await _ProductService.GetProductsWithCategoryByCategoryIdAsync(id);
+            return Ok(values);
+        }
     }
 }
