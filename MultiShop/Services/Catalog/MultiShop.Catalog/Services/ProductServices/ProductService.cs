951db1e107b3216395a956eb6abdd4eeb81e5467
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-10-04 18:51:33 +0300

    Service Method to Get Products by CategoryId has been Created

--- MultiShop/Services/Catalog/MultiShop.Catalog/Services/ProductServices/ProductService.cs
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Services/ProductServices/ProductService.cs
@@ -60,5 +60,15 @@ namespace MultiShop.Catalog.Services.ProductServices
             }
             return _mapper.Map<List<ResultProductsWithCategoryDto>>(values);
         }
+
+        public async Task<List<ResultProductsWithCategoryDto>> GetProductsWithCategoryByCategoryIdAsync(string categoryId)
+        {
+            var values = await _productCollection.Find(x => x.CategoryId == categoryId).ToListAsync();
+            foreach (var item in values)
+            {
+                item.Category = await _categoryCollection.Find<Category>(x => x.CategoryId == item.CategoryId).FirstAsync();
+            }
+            return _mapper.Map<List<ResultProductsWithCategoryDto>>(values);
+        }
     }
 }
