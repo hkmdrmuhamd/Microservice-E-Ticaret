c9428e6cb37a83431d6c736e0b85150dd2e2eb06
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-10-08 20:52:59 +0300

    Adding Product Images According to User Request

---
+++ MultiShop/Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductImageController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductImageDtos;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [AllowAnonymous]
+    [Area("Admin")]
+    [Route("Admin/ProductImage")]
+    public class ProductImageController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductImageController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        [HttpGet]
+        [Route("ProductImageDetail/{id}")]
+        public async Task<IActionResult> ProductImageDetail(string id)
+        {
+            ViewBag.V0 = "Ürün Görselleri İşlemleri";
+            ViewBag.V1 = "Ana Sayfa";
+            ViewBag.V2 = "Ürün Görselleri";
+            ViewBag.V3 = "Ürün Görselleri Listesi";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7000/api/ProductImages/ProductImagesByProductId?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateProductImageDto>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("ProductImageDetail/{id}")]
+        public async Task<IActionResult> ProductImageDetail(UpdateProductImageDto updateProductImageDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateProductImageDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7000/api/ProductImages", stringContent); //API'den girilmiş olan veri çekildi.
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ProductListWithCategory", "Product", new { area = "Admin" });
+            }
+
+            var errorResponse = await responseMessage.Content.ReadAsStringAsync();
+            ViewBag.Error = "API Error: " + errorResponse;
+
+            return View(updateProductImageDto);
+        }
+    }
+}
