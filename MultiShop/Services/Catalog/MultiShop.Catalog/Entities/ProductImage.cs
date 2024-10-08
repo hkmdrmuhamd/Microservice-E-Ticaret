0ee97312cfb3f0de562afdec703c9d0bce876153
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-10-08 20:45:10 +0300

    Product Image List

--- MultiShop/Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
@@ -8,9 +8,7 @@ namespace MultiShop.Catalog.Entities
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string ProductImageId { get; set; }
-        public string Image1 { get; set; }
-        public string Image2 { get; set; }
-        public string Image3 { get; set; }
+        public List<string> Image { get; set; } = new List<string>();
         public string ProductId { get; set; }
         [BsonIgnore]
         public Product Product { get; set; }
