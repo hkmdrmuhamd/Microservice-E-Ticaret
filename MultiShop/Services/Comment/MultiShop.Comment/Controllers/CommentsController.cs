0a4d62baced2ce8696534889af5283c58f258207
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-10-10 19:00:55 +0300

    CRUD Operations Created

---
+++ MultiShop/Services/Comment/MultiShop.Comment/Controllers/CommentsController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Comment.Context;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class CommentsController : ControllerBase
+    {
+        private readonly CommentContext _context;
+
+        public CommentsController(CommentContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult CommentList()
+        {
+            var values = _context.UserComments.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetComment(int id)
+        {
+            var value = _context.UserComments.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateComment(UserComment comment)
+        {
+            _context.UserComments.Add(comment);
+            _context.SaveChanges();
+            return Ok("Yorum başarıyla eklendi.");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateComment(UserComment comment)
+        {
+            _context.UserComments.Update(comment);
+            _context.SaveChanges();
+            return Ok("Yorum başarıyla güncellendi.");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteComment(int id)
+        {
+            var value = _context.UserComments.Find(id);
+            
+            if (value != null)
+            {
+                _context.UserComments.Remove(value);
+                _context.SaveChanges();
+                return Ok("Yorum başarıyla silindi.");
+            } else
+            {
+                return NotFound("Yorum bulunamadı.");
+            }
+        }
+    }
+}
