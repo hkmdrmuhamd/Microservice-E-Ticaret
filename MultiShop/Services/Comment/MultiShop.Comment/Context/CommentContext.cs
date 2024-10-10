94011415c11f3ccc96f9fb5ac933025bbb589d6c
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-10-10 18:37:56 +0300

    MySQL Connections Established

---
+++ MultiShop/Services/Comment/MultiShop.Comment/Context/CommentContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Context
+{
+    public class CommentContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=localhost;Port=3305;Database=MultiShopCommentDb;User=*****;Password=*****;";
+            optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(5, 7, 0)));
+        }
+
+        public DbSet<UserComment> UserComments { get; set; }
+    }
+}
