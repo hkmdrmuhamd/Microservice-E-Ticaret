99b1536602a2519c04f9cc6cdd92cba841611770
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-10-15 19:48:49 +0300

    Contact Operations

--- MultiShop/Frontends/MultiShop.WebUI/Controllers/ContactController.cs
+++ MultiShop/Frontends/MultiShop.WebUI/Controllers/ContactController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ContactDtos;
+using MultiShop.DtoLayer.CommentDtos;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDto createContactDto)
+        {
+            createContactDto.IsRead = false;
+            createContactDto.SendDate = DateTime.Now;
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7000/api/Contacts", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+
             return View();
         }
     }
