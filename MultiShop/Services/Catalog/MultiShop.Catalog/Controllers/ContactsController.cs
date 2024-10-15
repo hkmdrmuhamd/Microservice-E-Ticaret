06c52e6a3359f04b0f9851d0a8f35074b6953792
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-10-15 17:24:37 +0300

    Contact Controller Created

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Controllers/ContactsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ContactDtos;
+using MultiShop.Catalog.Services.ContactServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+
+        public ContactsController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetContactList()
+        {
+            var values = await _contactService.GetContactListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetContactByIdContact(string id)
+        {
+            var value = await _contactService.GetContactByIdAsync(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateContact(CreateContactDto createContactDto)
+        {
+            await _contactService.CreateContactAsync(createContactDto);
+            return Ok("İletişim bilgileri başarıyla eklendi.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateContact(UpdateContactDto updateContactDto)
+        {
+            await _contactService.UpdateContactAsync(updateContactDto);
+            return Ok("İletişim bilgileri başarıyla güncellendi.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteContact(string id)
+        {
+            await _contactService.DeleteContactAsync(id);
+            return Ok("İletişim bilgileri başarıyla silindi.");
+        }
+    }
+}
