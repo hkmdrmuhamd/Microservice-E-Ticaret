13416bccc8a635b86358f6c12196709168b349fe
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-10-15 17:23:48 +0300

    Contact Services Created

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Services/ContactServices/IContactService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.Catalog.Dtos.ContactDtos;
+
+namespace MultiShop.Catalog.Services.ContactServices
+{
+    public interface IContactService
+    {
+        Task<List<ResultContactDto>> GetContactListAsync();
+        Task<GetContactByIdContactDto> GetContactByIdAsync(string id);
+        Task CreateContactAsync(CreateContactDto createContactDto);
+        Task UpdateContactAsync(UpdateContactDto updateContactDto);
+        Task DeleteContactAsync(string id);
+    }
+}
