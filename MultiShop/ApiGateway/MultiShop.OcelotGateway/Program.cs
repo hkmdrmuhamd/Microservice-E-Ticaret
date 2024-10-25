38bde55ffab571f09a76dd2a93d57c7b0a2952e0
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-10-25 17:55:52 +0300

    Ocelot Authentication Key

--- MultiShop/ApiGateway/MultiShop.OcelotGateway/Program.cs
+++ MultiShop/ApiGateway/MultiShop.OcelotGateway/Program.cs
@@ -4,7 +4,7 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+builder.Services.AddAuthentication().AddJwtBearer("OcelotAuthenticationScheme", options =>
 {
     options.Authority = builder.Configuration["IdentityServerUrl"];
     options.Audience = "ResoruceOcelot";
