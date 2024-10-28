c963d2bc1bc7999203c3919ee8644f73936e6994
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-10-28 17:26:09 +0300

    Login Test Controller

--- MultiShop/Frontends/MultiShop.WebUI/Controllers/LoginController.cs
+++ MultiShop/Frontends/MultiShop.WebUI/Controllers/LoginController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using MultiShop.DtoLayer.IdentityDtos.LoginDtos;
 using MultiShop.WebUI.Models;
-using MultiShop.WebUI.Services;
 using MultiShop.WebUI.Services.Interfaces;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -12,7 +11,7 @@ using System.Text.Json;
 
 namespace MultiShop.WebUI.Controllers
 {
-	public class LoginController : Controller
+    public class LoginController : Controller
 	{
 		private readonly IHttpClientFactory _httpClientFactory;
 		private readonly ILoginService _loginService;
@@ -34,41 +33,7 @@ namespace MultiShop.WebUI.Controllers
 		[HttpPost]
 		public async Task<IActionResult> Index(CreateLoginDto createLoginDto)
 		{
-			var client = _httpClientFactory.CreateClient();
-			var content = new StringContent(JsonSerializer.Serialize(createLoginDto), Encoding.UTF8, "application/json");
-			var response = await client.PostAsync("http://localhost:5001/api/Logins", content);
-			if(response.IsSuccessStatusCode)
-			{
-				var jsonData = await response.Content.ReadAsStringAsync();
-				var tokenModel = JsonSerializer.Deserialize<JwtResponseModel>(jsonData, new JsonSerializerOptions //Modeli alır ve json'dan deserialize eder
-				{
-					// JSON'daki property adları camelCase ise, otomatik olarak modeldeki PascalCase adlara map'lenmesini sağlar
-					PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-				});
-
-				if(tokenModel != null)
-				{
-					// JwtSecurityTokenHandler sınıfını kullanabilmek için Microsoft.AspNetCore.Authentication.JwtBearer paketini yüklememiz gerekmektedir
-					JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler(); // JWT token'ını işleyecek bir handler oluşturur
-					var token = handler.ReadJwtToken(tokenModel.Token); // Token string'ini JWT objesine çevirir
-					var claims = token.Claims.ToList(); // JWT içindeki tüm claim'leri listeye dönüştürür (claim: token'daki kullanıcıya ait bilgiler)
-
-					if (tokenModel.Token != null)
-					{
-						claims.Add(new Claim("multishoptoken", tokenModel.Token)); //Token'i çözeceğimiz sayfalarda kullanmak için isim verip claim'lere kaydeder.
-						var claimsIdentity = new ClaimsIdentity(claims, JwtBearerDefaults.AuthenticationScheme); // JWT claim'lerini kullanarak bir kimlik oluşturur
-						var authProps = new AuthenticationProperties // Kimlik doğrulama özelliklerini ayarlar (token'ın bitiş süresi ve kalıcı olup olmayacağı)
-						{
-							ExpiresUtc = tokenModel.ExpireDate, // Token'ın ne zaman sona ereceğini ayarlar
-							IsPersistent = true // Kullanıcı oturumunun kalıcı olmasını sağlar
-						};
-
-						await HttpContext.SignInAsync(JwtBearerDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProps); // Kullanıcıyı belirlenen kimlik ve özelliklerle oturum açtırır (JWT ile kimlik doğrulaması yapılır)
-						var id = _loginService.GetUserId;
-						return RedirectToAction("Index", "Default");
-					}
-				}
-			}
+			
 			return View();
 		}
 
@@ -81,10 +46,10 @@ namespace MultiShop.WebUI.Controllers
 		//[HttpPost]
 		public async Task<IActionResult> SignIn(SignInDto signInDto)
 		{
-            signInDto.Username = "deneme";
-            signInDto.Password = "1234";
+            signInDto.Username = "hkmdr";
+            signInDto.Password = "1111aA*";
 			await _identityService.SignIn(signInDto);
-            return RedirectToAction("Index", "Test");
+            return RedirectToAction("Index", "User");
         }
 	}
 }
