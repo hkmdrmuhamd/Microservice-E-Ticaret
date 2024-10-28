ffb1aaea44c3970a6b8531d70b9111f1e38fc23e
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-10-28 17:27:11 +0300

    Handler Created

---
+++ MultiShop/Frontends/MultiShop.WebUI/Handlers/ResourceOwnerPasswordTokenHandler.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using MultiShop.WebUI.Services.Interfaces;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace MultiShop.WebUI.Handlers
+{
+    public class ResourceOwnerPasswordTokenHandler : DelegatingHandler
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IIdentityService _identityService;
+
+        public ResourceOwnerPasswordTokenHandler(IHttpContextAccessor httpContextAccessor, IIdentityService identityService)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _identityService = identityService;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var accessToken = await _httpContextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);//Access token alındı
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var response = await base.SendAsync(request, cancellationToken);
+
+            if(response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                var tokenResponse = await _identityService.GetRefreshToken(); //Yeni token üretildi
+                if(tokenResponse != null)
+                {
+                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken); //Yeni token requeste eklendi
+                    response = await base.SendAsync(request, cancellationToken); //Request tekrar gönderildi
+                }
+            }
+
+            if(response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                //hata mesajı
+            }
+
+            return response;
+        }
+    }
+}
