e98c562b703a742b618a915f44f9b3b8044bb976
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-11-19 22:11:08 +0300

    Code Refactoring

--- MultiShop/Frontends/MultiShop.WebUI/Controllers/LoginController.cs
+++ MultiShop/Frontends/MultiShop.WebUI/Controllers/LoginController.cs
@@ -14,13 +14,11 @@ namespace MultiShop.WebUI.Controllers
     public class LoginController : Controller
 	{
 		private readonly IHttpClientFactory _httpClientFactory;
-		private readonly ILoginService _loginService;
 		private readonly IIdentityService _identityService;
 
-        public LoginController(IHttpClientFactory httpClientFactory, ILoginService loginService, IIdentityService identityService)
+        public LoginController(IHttpClientFactory httpClientFactory, IIdentityService identityService)
         {
             _httpClientFactory = httpClientFactory;
-            _loginService = loginService;
             _identityService = identityService;
         }
 
@@ -31,25 +29,10 @@ namespace MultiShop.WebUI.Controllers
 		}
 
 		[HttpPost]
-		public async Task<IActionResult> Index(CreateLoginDto createLoginDto)
+		public async Task<IActionResult> Index(SignInDto signInDto)
 		{
-			
-			return View();
-		}
-
-		//[HttpGet]
-		//public IActionResult SignIn()
-		//{             
-		//	return View();        
-		//}
-
-		//[HttpPost]
-		public async Task<IActionResult> SignIn(SignInDto signInDto)
-		{
-            signInDto.Username = "hkmdr";
-            signInDto.Password = "1111aA*";
 			await _identityService.SignIn(signInDto);
-            return RedirectToAction("Index", "User");
-        }
+			return RedirectToAction("Index", "User");
+		}
 	}
 }
--- MultiShop/IdentityServer/MultiShop.IdentityServer/Config.cs
+++ MultiShop/IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -55,7 +55,11 @@ namespace MultiShop.IdentityServer
                 ClientName = "Multi Shop Visitor User",
                 AllowedGrantTypes = GrantTypes.ClientCredentials,
                 ClientSecrets = { new Secret("multishopsecret".Sha256()) },
-                AllowedScopes = { "CatalogFullPermission", "CatalogReadPermission", "OcelotFullPermission", "CommentFullPermission", "ImagesFullPermission" }
+                AllowedScopes = { 
+                    "CatalogFullPermission", "CatalogReadPermission", "OcelotFullPermission",
+                    "CommentFullPermission", "ImagesFullPermission",
+                    IdentityServerConstants.LocalApi.ScopeName
+                }
             },
 
             //Manager
